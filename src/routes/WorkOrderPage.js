e34ae85b6f6c3fbe08084343728efeee9a741350
管梦秋 <mf1732033@smail.nju.edu.cn>
2017-12-10 17:22:29 +0800

    select

--- src/routes/WorkOrderPage.js
+++ src/routes/WorkOrderPage.js
@@ -1,11 +1,69 @@
 //单个WorkOrder
 import React from 'react';
 import { connect } from 'dva';
-import { Timeline, Icon } from 'antd';
+import { Timeline, Icon ,Select} from 'antd';
+import { Cascader } from 'antd';
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
+
+const options = [{
+  value: 'zhejiang',
+  label: 'Zhejiang',
+  children: [{
+    value: 'hangzhou',
+    label: 'Hangzhou',
+    children: [{
+      value: 'xihu',
+      label: 'West Lake',
+    }],
+  }],
+}, {
+  value: 'jiangsu',
+  label: 'Jiangsu',
+  disabled: true,
+  children: [{
+    value: 'nanjing',
+    label: 'Nanjing',
+    children: [{
+      value: 'zhonghuamen',
+      label: 'Zhong Hua Men',
+    }],
+  }],
+}];
+
+function getOptions(categories){
+  let options = [];
+  categories.forEach(function (category) {
+    let children = [];
+    if(category.children&&category.children!=null&&category.children.length!=0) {
+      category.children.forEach(function (child) {
+          children.push({
+            value: child.id,
+            label: child.nodeName
+          });
+        }
+      );
+    }
+    options.push({
+      value:category.id,
+      label:category.nodeName,
+      children:children
+    });
+    }
+  );
+  return options;
+}
+function onChange(value) {
+  console.log(value);
+}
 
 class WorkOrderPage extends React.Component{
 
-  renderPhotos(photos){
+  renderPhotos(photos,categories){
+    console.log("ball",categories)
+    let that = this;
 
     if(photos==null||photos.length==0){
       return (<span/>)
@@ -13,20 +71,23 @@ class WorkOrderPage extends React.Component{
     let photoWall =[];
     photos.forEach((photo)=>{
       photoWall.push(
-        <img src = {photo.url} style={{marginRight: '10px',marginTop:'5px'}} key={photo.id} />
+        <div>
+          <img src = {photo.url} style={{marginRight: '10px',marginTop:'5px'}} key={photo.id} />
+          <Cascader options={getOptions(categories)} onChange={onChange}/>
+        </div>
       )
     });
     return photoWall;
 
   }
 
-  renderWorkOrder({photos,description,time}){
+  renderWorkOrder({photos,description,time,categories}){
     return (
       <Timeline.Item dot={<Icon type="user" style={{fontSize: '16px'}}/>} color="blue"  key={1}>
         <div>
           <span color="blue">{time}</span><br/>
           {description}<br/>
-          {this.renderPhotos(photos)}
+          {this.renderPhotos(photos,categories)}
        </div>
      </Timeline.Item>
     )
@@ -67,11 +128,12 @@ class WorkOrderPage extends React.Component{
 
   render(){
     const { description, photos, time, sequences} =this.props.workOrder;
+    const { categories } = this.props.category;
 
     return (
       <div style={{padding:'20px'}}>
         <Timeline>
-          {this.renderWorkOrder({description,photos,time})}
+          {this.renderWorkOrder({description,photos,time,categories})}
           {this.renderSequence(sequences)}
         </Timeline>
     </div>
@@ -79,8 +141,8 @@ class WorkOrderPage extends React.Component{
   }
 }
 
-function mapStateToProps({workOrder}) {
-  return {workOrder};
+function mapStateToProps({workOrder,category}) {
+  return {workOrder,category};
 }
 
 export default connect(mapStateToProps)(WorkOrderPage);
